d2e4b65ebeca7183f2d05fc08ff9276eb3f4f62a
yumiykim <ymkim030403@gmail.com>
2025-07-01 05:12:53 +0900

    여름학기 1주차 세션 과제

---
+++ project/singer/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('singers/', views.singer_list),
+    path('singers/<int:pk>/', views.singer_detail),
+    path('singers/<int:singer_id>/songs/', views.song_create),
+    path('songs/<int:song_id>/comments/', views.comment_list),
+    path('songs/<int:song_id>/comments/new/', views.comment_create),
+]
