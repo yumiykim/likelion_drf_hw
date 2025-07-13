c338df359f328360c515d1933c03964e08f8e320
yumiykim <ymkim030403@gmail.com>
2025-07-13 21:05:54 +0900

    여름학기 2주차 DRF 과제

--- project/singer/models.py
+++ project/singer/models.py
@@ -1,9 +1,18 @@
 from django.db import models
+import os
+
+def image_upload_path(instance, filename):
+    return os.path.join(str(instance.id), filename)
+
+class Tag(models.Model):
+    name = models.CharField(max_length=20, unique=True)
 
 class Singer(models.Model):
     name = models.CharField(max_length=100, default="가수 미지정")
     content = models.TextField()
     debut = models.DateField()
+    image = models.ImageField(upload_to=image_upload_path, blank=True, null=True)
+    tags = models.ManyToManyField(Tag, blank=True)
 
 class Song(models.Model):
     singer = models.ForeignKey(Singer, on_delete=models.CASCADE, related_name='songs')
